bbb45c0461400c6d27c1bc1d98311174313281c6
Lynn Muswere <lynn.muswere@sky.uk>
2023-04-28 14:08:41 +0100

    Initial commit

---
+++ src/test/java/com/lynnmuswere/Aurora/AuroraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynnmuswere.Aurora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
